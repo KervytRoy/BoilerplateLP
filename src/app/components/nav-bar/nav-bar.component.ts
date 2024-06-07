6f2ac49e1adb40d5e098a96342785e74488acdf7
kervyt <kervytroy96@gmail.com>
2024-06-07 13:07:13 -0400

    cambios navbar responsive

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('asDivNav', { static: true }) asDivNav!: ElementRef;
+
+  ngAfterViewInit() {
+    const divNav = this.asDivNav.nativeElement;
+    const toggleButton = divNav.querySelector('#toggle-menu') as HTMLElement; // Cambio a querySelector
+    const navMobile = divNav.querySelector('#nav-mobile') as HTMLElement; // Cambio a querySelector
+
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.scrollY;
+
+      // Ajustar la posición según la visibilidad
+      if (scrollTop > 100) {
+        divNav.style.position = 'fixed';
+        divNav.style.zIndex = '999';
+        divNav.style.animation = 'slide-down 0.8s ease forwards';
+      } else {
+        divNav.style.position = 'block'; // Otra posición deseada
+        divNav.style.animation = 'none';
+      }
+    });
+
+    toggleButton.addEventListener('click', () => {
+      navMobile.classList.toggle('active');
+    });
+  }
 
 }
