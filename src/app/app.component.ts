25ff65efff8d7c35e761f3ff4bca964aaca24361
ronald <ron_developing@outlook.es>
2024-09-22 14:21:14 -0400

    formulario correro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PresentationComponent } from './presentation/presentation.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { WhoWeAreComponent } from './components/who-we-are/who-we-are.component';
 import { OurValuesComponent } from './components/our-values/our-values.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -20,6 +21,7 @@ import { OurServicesComponent } from './components/our-services/our-services.com
     NavBarComponent,
     HeaderComponent,
     FlayersComponent,
+    ContactFormComponent,
     OurServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
